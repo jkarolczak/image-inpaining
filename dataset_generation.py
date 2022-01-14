07302573226e2eb1496f33a1f29534105a9ab409
Konrad <konszewczyk@outlook.com>
2022-01-14 20:04:00 +0100

    Revamped data generation, 5000 images

--- dataset_generation.py
+++ dataset_generation.py
@@ -5,7 +5,7 @@ import cv2
 import pandas as pd
 
 from src.data_generator import GeneratedDataset
-from src.visualization import transformToImage
+from src.visualization import transform_to_image
 
 
 if __name__ == "__main__":
@@ -17,8 +17,7 @@ if __name__ == "__main__":
 
     target_path = "data/img/target"
     input_path = "data/img/input"
-    local_path = "data/img/local"
-    paths = [target_path, input_path, local_path]
+    paths = [target_path, input_path]
 
     os.makedirs("data/img", exist_ok=True)
 
@@ -31,17 +30,23 @@ if __name__ == "__main__":
                 continue
             os.remove(file_path)
 
-    image_ids = {'image_id': []}
+    image_ids = {'image_id': [], 'x': [], 'y': [], 'width': [], 'height': []}
 
     dataGenerator = GeneratedDataset(seed = seed, dataset_size = dataset_size)
 
-    for (img_name, target_img, input_img, local_img) in dataGenerator:
+    for (img_name, target_img, input_img, boundary) in dataGenerator:
+        x, y, w, h = boundary
         image_ids['image_id'].append(img_name)
-        imgs = [target_img, input_img, local_img]
+        image_ids['x'].append(x)
+        image_ids['y'].append(y)
+        image_ids['width'].append(w)
+        image_ids['height'].append(h)
+
+        imgs = [target_img, input_img]
 
         for (img, path) in zip(imgs, paths):
             file_path = os.path.join(path, img_name)
-            img = transformToImage(img)
+            img = transform_to_image(img)
             cv2.imwrite(file_path, img)
     
     image_df = pd.DataFrame(image_ids)
--- src/data_generator.py
+++ src/data_generator.py
@@ -14,6 +14,10 @@ class GeneratedDataset(torch.utils.data.Dataset):
         image_dir: str = 'img/',
         item_list: str = 'dataset.csv',
         filename_col: str = 'image_id',
+        x1_col: str = 'x_1',
+        y1_col: str = 'y_1',
+        width_col: str = 'width',
+        height_col: str = 'height',
         dataset_size: int = 25000,
         snippet_size: Union[int, Tuple[int, int], Tuple[Tuple[int, int], Tuple[int, int]]] = ((25, 55), (25, 55)),
         seed: int = 23
@@ -25,9 +29,11 @@ class GeneratedDataset(torch.utils.data.Dataset):
         self.dir = dir
         self.image_dir = image_dir
         self.dataset_size = dataset_size
-        item_list = pd.read_csv(path)[filename_col]
-        self.item_list = self.rng.choice(item_list, size=self.dataset_size, replace=False, shuffle=False)
+        self.item_df = pd.read_csv(path)[filename_col]
+        self.item_list = self.rng.choice(self.item_df, size=self.dataset_size, replace=False, shuffle=False)
         self.snippet_size = snippet_size
+
+        self.filename_col = filename_col
         
     def __len__(self) -> int:
         return self.dataset_size
@@ -36,9 +42,9 @@ class GeneratedDataset(torch.utils.data.Dataset):
         img_name = self.item_list[idx]
         img_path = os.path.join(self.dir, self.image_dir, img_name)
         target_img = cv2.imread(img_path)
-        input_img, local_img = self._remove_snippet(target_img)
-        
-        return (img_name, target_img, input_img, local_img)
+        input_img, local_boundary = self._remove_snippet(target_img)
+
+        return (img_name, target_img, input_img, local_boundary)
     
     def _remove_snippet(self, image: np.ndarray) -> np.ndarray:
         img = image.copy()
@@ -51,9 +57,8 @@ class GeneratedDataset(torch.utils.data.Dataset):
             else:
                 width = self.rng.integers(*size[0])
                 height = self.rng.integers(*size[1])
-        y = 30 + self.rng.integers(img.shape[1] - height - 60)        
-        x = 30 + self.rng.integers(img.shape[1] - width - 60)
-        snipped = img[y:y + height, x:x + width].copy()
+        y = 45 + self.rng.integers(image.shape[0] - height - 90)        
+        x = 35 + self.rng.integers(image.shape[1] - width - 70)
         img[y:y + height, x:x + width] = np.array([255, 255, 255])
-        return (img, snipped)
+        return (img, (x, y, width, height))
     
